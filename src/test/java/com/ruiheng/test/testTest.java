3386f401e46ee01783f490a40d69971ac1c4d593
RuiHengZhang1996 <zhangruiheng1996@gmail.com>
2021-03-15 18:19:29 -0400

    1

--- src/test/java/com/ruiheng/test/testTest.java
+++ src/test/java/com/ruiheng/test/testTest.java
@@ -5,29 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.ruiheng.spring5.config.SpringConfig;
-import com.ruiheng.spring5.service.UserService;
-
 public class testTest {
 
-	@Test
-	public void testService() {
-		ApplicationContext context = 
-				new ClassPathXmlApplicationContext("bean1.xml");
-		
-		UserService userService = context.getBean("userService",UserService.class);
-		userService.add();
-	}
-	
-	
-	@Test
-	public void testService1() {
-		
-		ApplicationContext context = 
-				new AnnotationConfigApplicationContext(SpringConfig.class);
-		
-		UserService userService = context.getBean("userService",UserService.class);
-		userService.add();
-	}
 	
 }
